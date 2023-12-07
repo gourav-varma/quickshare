71ffd789e944890c444170b3994558533bd88ef0
gourav-varma <gouravswork@gmail.com>
2023-12-07 17:18:30 +0530

    basic websocket setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let ws: WebSocket;
+
+  const establishWebsocketConnection = () => {
+    try {
+      ws = new WebSocket("ws://localhost:8080/echo");
+      console.log(ws)
+      ws.onopen = () =>  {
+        console.log("OPEN");
+      };
+      ws.onclose = () => {
+        console.log("CLOSE");
+      };
+      ws.onmessage = (res) => {
+        console.log(`response recieved ${res.data}`);
+      };
+    } catch (error) {
+      console.log(`Error while Establshing WebSocket Connection: ${error}`);
+    }
+  };
+
+  const sendHello = () => {
+    try {
+      if(ws){
+        ws.send("Hello");
+      }else {
+        throw new Error("No WebSocket Connection");
+      }
+    } catch (error) {
+      console.error(`Error while sending hello: ${error}`);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Socket testing</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={establishWebsocketConnection}>
+          Establish Socket Connection
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={sendHello}>Send Hello</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
